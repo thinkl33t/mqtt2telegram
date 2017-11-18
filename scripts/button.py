d3882d2ee27cae42dbebc0ad6aec6896c216f14e
Conor Riches <conor.riches@bbc.co.uk>
2017-11-18 00:09:25 +0000

    remove unrequired code

--- scripts/button.py
+++ scripts/button.py
@@ -11,19 +11,14 @@ with open('config.yaml') as config_f:
     config = yaml.safe_load(config_f)
 
 u = Updater(config['telegram']['token'])
-for file in os.listdir('scripts/'):
-    command = file.split('.', 1)
-    u.dispatcher.add_handler(CommandHandler(command[0], run_script))
 u.start_polling()
-
 
 mqttc = mqtt.Client(config['mqtt']['name'])
 mqttc.connect(config['mqtt']['server'])
 mqttc.publish("button/big/red/state", payload='pressed', qos=2, retain=True)
 send_to_bot(":tada: Big Red Button pressed... :poop:")
 
-
 def send_to_bot(message, increment=False):
-    global u, config, lastmsg
+    global u, config
     m = u.bot.sendMessage(chat_id=config['telegram']['chat_id'], text=message, parse_mode=ParseMode.MARKDOWN, disable_notification=True)
 
