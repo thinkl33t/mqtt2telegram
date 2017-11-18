316e5ade3b2229088aa0b5b9b972e89c321539a6
Conor Riches <conor.riches@bbc.co.uk>
2017-11-18 00:03:39 +0000

    big red button script

---
+++ scripts/button.py
@@ -0,0 +1,29 @@
+#!../venv/bin/python
+
+import sys
+import paho.mqtt.client as mqtt
+from telegram.ext import Updater, CommandHandler
+from telegram import ParseMode, ChatAction
+
+print (sys.argv[1:])
+
+with open('config.yaml') as config_f:
+    config = yaml.safe_load(config_f)
+
+u = Updater(config['telegram']['token'])
+for file in os.listdir('scripts/'):
+    command = file.split('.', 1)
+    u.dispatcher.add_handler(CommandHandler(command[0], run_script))
+u.start_polling()
+
+
+mqttc = mqtt.Client(config['mqtt']['name'])
+mqttc.connect(config['mqtt']['server'])
+mqttc.publish("button/big/red/state", payload='pressed', qos=2, retain=True)
+send_to_bot(":tada: Big Red Button pressed... :poop:")
+
+
+def send_to_bot(message, increment=False):
+    global u, config, lastmsg
+    m = u.bot.sendMessage(chat_id=config['telegram']['chat_id'], text=message, parse_mode=ParseMode.MARKDOWN, disable_notification=True)
+
